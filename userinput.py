88a8383a5327a4996a3a893bbf6371aae79d56aa
Rob Sullivan <5844677+rob-sullivan@users.noreply.github.com>
2020-09-22 19:34:12 +0100

    Update userinput.py

--- userinput.py
+++ userinput.py
@@ -1,8 +1,8 @@
-n1=input("Enter a number: ") #input 4
+n1=input("Enter a number:") #input 4
 print(n1+n1)
 #output is 44 because python interprets input as a string
 
-n1=input("Enter a number: ") #input 4
+n1=input("Enter a number:") #input 4
 n2 = int(n1)
 print(n2+n2)
 #output will now be 8
