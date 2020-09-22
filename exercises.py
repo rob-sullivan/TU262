8d58ae40a2ab033b2e7563d6c420b5253147a48f
Rob Sullivan <5844677+rob-sullivan@users.noreply.github.com>
2020-09-22 19:40:11 +0100

    Create exercises.py
    init

---
+++ exercises.py
@@ -0,0 +1,38 @@
+#22nd Sep 2020
+#OOSD
+#Svetlana Hensman
+
+from math import pi
+r = 12
+area = pi * r ** 2
+print("The area of a circle with radius", r , "is", area) #output needs to be rounded to two decimal places
+
+age = 20
+print(age*2)
+#output will be 40
+age = 20
+print(age + age)
+#output will be 40
+age = '20'
+print(age + age)
+#output will be 2020
+
+age='20'
+print("Your age is", age )
+
+age='30'
+print("Your age is", age )
+
+age=input("Please enter your age? ")
+
+print("Your age is", age)
+
+ 
+n1=input("Enter a number:") #input 4
+print(n1+n1)
+#output is 44 because python interprets input as a string
+
+n1=input("Enter a number:") #input 4
+n2 = int(n1)
+print(n2+n2)
+#output will now be 8
