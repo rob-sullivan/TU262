c348a37e30af311cf6694c925b1a91d0b8ec588c
Rob Sullivan <5844677+rob-sullivan@users.noreply.github.com>
2020-09-29 19:06:38 +0100

    Create checknumbers.py

---
+++ checknumbers.py
@@ -0,0 +1,10 @@
+first = int(input("Enter the first number:"))
+second = int(input("Enter the second number:"))
+
+if(first==second):
+  print("Both first and second are the same.")
+else:
+  if first > second:
+    print("First is bigger")
+  else:
+    print("Second is bigger")
